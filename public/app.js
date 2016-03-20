6155056d6872dea72a70c30ec0ad4920ab5c5024
thanawat <tha_chookes@hotmail.com>
2016-03-21 06:14:21 +0700

    done

--- public/app.js
+++ public/app.js
@@ -2,18 +2,57 @@
 angular.module('app', [])
   .controller('TodoListController', function () {
     var app = this
-    app.test = 'koy'
-    app.bookSelected = []
     app.price = 0
-    app.addBook = function (img, title, price) {
+    app.bookSelected = []
+    app.minQty = 0
+    app.discountTotal = 0
+    app.total = 0
+    app.addBook = function (no, title, price) {
       var jsonBook = {
-        'img': img,
+        'no': no,
         'title': title,
         'price': price
       }
-      app.bookSelected.push(jsonBook)
-      app.price += price
-      console.log(app.price, title)
+      if (app.bookSelected.length === 0) {
+        jsonBook.qty = 1
+        app.bookSelected.push(jsonBook)
+      } else {
+        var check = false
+        for (var i = 0; i < app.bookSelected.length; i++) {
+          if (app.bookSelected[i].no === no) {
+            check = true
+            app.bookSelected[i].qty += 1
+          }
+        }
+      }
+      if (check === false) {
+        jsonBook.qty = 1
+        app.bookSelected.push(jsonBook)
+      }
+      app.price = app.sumPrice()
+      app.minQty = app.findMinQty()
+      app.discountTotal = app.discount()
+      app.total = app.sumPrice() - app.discount()
+    }
+    app.sumPrice = function () {
+      this.sumQty = 0
+      this.sumP = 0
+      for (var i = 0; i < app.bookSelected.length; i++) {
+        this.sumP += app.bookSelected[i].qty * app.bookSelected[i].price
+      }
+      return this.sumP
+    }
+    app.findMinQty = function () {
+      this.min = app.bookSelected[0].qty
+      for (var i = 0; i < app.bookSelected.length; i++) {
+        if (app.bookSelected[i].qty < this.min) {
+          this.min = app.bookSelected[i].qty
+        }
+      }
+      return this.min
+    }
+    app.discount = function () {
+      return (app.bookSelected.length * ((app.bookSelected.length - 1) * 10) * app.minQty)
     }
     app.delBook = function (index) {
       app.bookSelected.splice(index, 1)
