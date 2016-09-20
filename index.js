c055660d50b39b3375fd5930da542a738c059c87
thanawat <tha_chookes@hotmail.com>
2016-09-21 02:48:45 +0700

    add api for IoT

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 
 app.use(express.static('public'))
+
+app.post('/koy', (req, res) => {
+  console.log(req)
+})
+
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
